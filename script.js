cfc0398c3c8c493f91dc7c31cca9585f12cc83dc
Chickenman333 <156724436+Chickenman333@users.noreply.github.com>
2024-02-03 23:06:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
+const gif = document.querySelector(".gif"); // Assuming this is the first GIF
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yipee!";
-  gif.src =
-    "https://i.pinimg.com/originals/91/bb/e5/91bbe5c5c9d32d755661e2cde47107e5.gif";
+  
+  // Temporarily store the current size of the first GIF
+  const gifWidth = gif.offsetWidth;
+  const gifHeight = gif.offsetHeight;
+
+  gif.src = "https://i.pinimg.com/originals/91/bb/e5/91bbe5c5c9d32d755661e2cde47107e5.gif";
+
+  // After changing the source, adjust the size of the second GIF to match the first one
+  gif.onload = () => {
+    gif.style.width = gifWidth + 'px';
+    gif.style.height = gifHeight + 'px';
+  };
+  
+  // Hide both buttons
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
 });
 
 noBtn.addEventListener("mouseover", () => {
@@ -21,3 +35,4 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
